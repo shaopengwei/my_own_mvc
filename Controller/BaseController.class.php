27cf5236c16f813ba933eff7fe82a5217fedcb81
shao <shaopengwei@hotmail.com>
2016-01-20 10:49:36 +0800

    2016-1-20 commit

---
+++ Controller/BaseController.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 这是控制器的基类
+ * @author shaopengwei@hotmail.com
+ * @since 2016-1-19
+ */
+class BaseController{
+
+	public function test(){
+		echo 'this is basecontroller.'.DB_HOST;
+	}
+}
